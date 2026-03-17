e6c612dbe131e9bad18c80e4cc435a555230606b
rafasaher-gif <rafasaher@gmail.com>
2026-03-16 21:06:42 -0300

    Update masterPasswordMiddleware.js

--- middlewares/masterPasswordMiddleware.js
+++ middlewares/masterPasswordMiddleware.js
@@ -1,1 +1,10 @@
 
+exports.checkMasterPassword = (req, res, next) => {
+  const { master_password } = req.body
+
+  if (!master_password || master_password !== process.env.MASTER_PASSWORD) {
+    return res.status(403).send('Senha master inválida')
+  }
+
+  return next()
+}
