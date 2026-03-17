2de755674d14ce2c76524ef823fc942ab1fd407b
rafasaher-gif <rafasaher@gmail.com>
2026-03-16 21:04:51 -0300

    Update ocorrenciaRoutes.js

--- routes/ocorrenciaRoutes.js
+++ routes/ocorrenciaRoutes.js
@@ -1,1 +1,11 @@
 
+const express = require('express')
+const router = express.Router()
+const ocorrenciaController = require('../controllers/ocorrenciaController')
+const { isAuthenticated } = require('../middlewares/authMiddleware')
+
+router.get('/', isAuthenticated, ocorrenciaController.lista)
+router.get('/nova', isAuthenticated, ocorrenciaController.novaPage)
+router.get('/:id', isAuthenticated, ocorrenciaController.detalhe)
+
+module.exports = router
