cbe0407775e567f8db79fa8509caf8f3d6b3d374
rafasaher-gif <rafasaher@gmail.com>
2026-03-16 21:05:02 -0300

    Update relatorioRoutes.js

--- routes/relatorioRoutes.js
+++ routes/relatorioRoutes.js
@@ -1,1 +1,9 @@
 
+const express = require('express')
+const router = express.Router()
+const relatorioController = require('../controllers/relatorioController')
+const { isAuthenticated } = require('../middlewares/authMiddleware')
+
+router.get('/', isAuthenticated, relatorioController.index)
+
+module.exports = router
