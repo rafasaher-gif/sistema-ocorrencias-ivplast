cce93667c5e6ef13f404cb939f39a21009ee8e5f
rafasaher-gif <rafasaher@gmail.com>
2026-03-16 21:06:31 -0300

    Update roleMiddleware.js

--- middlewares/roleMiddleware.js
+++ middlewares/roleMiddleware.js
@@ -1,1 +1,14 @@
 
+exports.requireRole = (rolesPermitidos = []) => {
+  return (req, res, next) => {
+    if (!req.session.usuario) {
+      return res.redirect('/login')
+    }
+
+    if (!rolesPermitidos.includes(req.session.usuario.perfil)) {
+      return res.status(403).send('Acesso negado')
+    }
+
+    return next()
+  }
+}
