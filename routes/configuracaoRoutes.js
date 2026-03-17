45012c49edf7630ac7e06608d875a78175c0ed8f
rafasaher-gif <rafasaher@gmail.com>
2026-03-16 21:05:14 -0300

    Update configuracaoRoutes.js

--- routes/configuracaoRoutes.js
+++ routes/configuracaoRoutes.js
@@ -1,1 +1,10 @@
 
+const express = require('express')
+const router = express.Router()
+const configuracaoController = require('../controllers/configuracaoController')
+const { isAuthenticated } = require('../middlewares/authMiddleware')
+const { requireRole } = require('../middlewares/roleMiddleware')
+
+router.get('/', isAuthenticated, requireRole(['Administrador']), configuracaoController.index)
+
+module.exports = router
