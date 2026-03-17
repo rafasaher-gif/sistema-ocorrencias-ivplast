24a8f0629c2f39e8654025ed03227bb440a60777
rafasaher-gif <rafasaher@gmail.com>
2026-03-16 21:05:44 -0300

    Update ocorrenciaController.js

--- controllers/ocorrenciaController.js
+++ controllers/ocorrenciaController.js
@@ -1,1 +1,12 @@
 
+exports.lista = async (req, res) => {
+  return res.render('lista_ocorrencias')
+}
+
+exports.novaPage = async (req, res) => {
+  return res.render('nova_ocorrencia')
+}
+
+exports.detalhe = async (req, res) => {
+  return res.render('acompanhamento_ocorrencia', { id: req.params.id })
+}
