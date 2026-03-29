ee6ae09fc78084e7422b496ddb217410e7be523b
rafasaher-gif <rafasaher@gmail.com>
2026-03-29 13:49:01 -0300

    Update ocorrenciaRoutes.js

--- routes/ocorrenciaRoutes.js
+++ routes/ocorrenciaRoutes.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const router = express.Router()
 const ocorrenciaController = require('../controllers/ocorrenciaController')
@@ -6,6 +5,8 @@ const { isAuthenticated } = require('../middlewares/authMiddleware')
 
 router.get('/', isAuthenticated, ocorrenciaController.lista)
 router.get('/nova', isAuthenticated, ocorrenciaController.novaPage)
+router.post('/nova', isAuthenticated, ocorrenciaController.criar)
+router.get('/produtos', isAuthenticated, ocorrenciaController.produtosPorEmpresa)
 router.get('/:id', isAuthenticated, ocorrenciaController.detalhe)
 
 module.exports = router
